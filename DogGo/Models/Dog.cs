9407664b5068fd6fe34ed44439af5b9739b3611d
Austin Parker <austincparker@gmail.com>
2022-03-19 13:20:06 -0500

    Get all dogs and view complete

--- DogGo/Models/Dog.cs
+++ DogGo/Models/Dog.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string OwnerId { get; set; }
+        public int OwnerId { get; set; }
+        public string Breed { get; set; }
     }
 }
