3127e0e3d218df5903a4b65506eea57a6188c5a3
Austin Parker <austincparker@gmail.com>
2022-03-19 09:57:23 -0500

    made dog model

---
+++ DogGo/Models/Dog.cs
@@ -0,0 +1,9 @@
+﻿namespace DogGo.Models
+{
+    public class Dog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string OwnerId { get; set; }
+    }
+}
